0aacc8d9f2b0914d0a8f6c4252c88098c0d17841
kimerajz <gabrielepiccardo.96@gmail.com>
2023-02-20 09:46:26 +0100

    given a rapid description of every function

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -7,28 +7,28 @@ import numpy as np
 import pandas
 
 
+#assignining to each path a variable
 videos_path= "C:/Users/gabri/desktop/roba_seria/Python/Dataset/archive/top_view/videos"
 csv_path = "C:/Users/gabri/desktop/roba_seria/Python/Dataset/archive/top_view/annotations"
 output_path= "C:/Users/gabri/desktop/roba_seria/Python/Dataset/archive/top_view/yolo_annotations"
 
 
-
+#check if the output destination folder already exists, if not it creates one
 def checkFolder (folderFrame_path):
     if not os.path.exists(folderFrame_path):
         os.makedirs(folderFrame_path)
 
 
-def pathVideoCsv (csv_path):
+#return the single path of every csv receiving the csv folder path
+def singleCsv (csv_path):
     csv_List = os.listdir(csv_path)
     for csv in csv_List:
         csv_file = csv_path + '/' + csv
+
         return csv_file
 
 
-#def makeRectDir (outputImages_path, outputImagesRect_path):
-
-
-
+#for every csv file given, read it and save in a list the row
 def extractData (csv_file):
     with open(csv_file, 'r') as file:
         reader = csv.reader(file)
@@ -43,19 +43,18 @@ def extractData (csv_file):
     return csv_lines
 
 
+#for every line of the csv file, calculate the right dimension of the box and save in a txt
 def saveBoundingBoxes (csv_lines, output_path):
     for line in csv_lines:
+        #first element of the csv list is always the class
         id = line[0]
         bounding_boxes = line[1:]
         txt_name = output_path + "/" + id + ".txt"
         with open(txt_name, 'w') as file:
             image_name1 = txt_name.replace(".txt", ".png")
             image_name = image_name1.replace("txts", "images")
-            print (image_name)
-            #converto il nome del file nel nome dell immagine
             img = cv2.imread(image_name, 4)
             H, W, C = img.shape
-            #imread dell immagine e ne calcolo la shape
             for i in range (0, len(bounding_boxes), 4):
                 bb_height = float(float(bounding_boxes[i])/H)
                 bb_left = float(float(bounding_boxes[i+1])/W)
@@ -71,6 +70,7 @@ def saveBoundingBoxes (csv_lines, output_path):
             print("Saved " + f"{id}.txt")
 
 
+#for each video taken in input save the frames based on the refresh frequency
 def video2Frames (video_path, output_path):
     cap = cv2.VideoCapture(video_path)
     index = 0
@@ -89,18 +89,17 @@ def video2Frames (video_path, output_path):
 
 
 def csv2txt (csv_path, output_path):
-    csv_file = pathVideoCsv(csv_path)
+    csv_file = singleCsv(csv_path)
     csv_lines = extractData(csv_file)
     saveBoundingBoxes(csv_lines, output_path)
 
     return
 
 
-
-
-
+#build up a list containing every video path
 list_video = os.listdir(videos_path)
 for video_name in list_video:
+    #extract the name of every video due to give the name of every output folder
     file_name = video_name.replace(".mp4", "")
     csv_file = video_name.replace("mp4", "csv")
     outputImages_path = output_path + "/images/" + file_name
@@ -109,23 +108,3 @@ for video_name in list_video:
     checkFolder(outputTxt_path)
     video2Frames(videos_path + "/" + video_name, outputImages_path)
     csv2txt(csv_path, outputTxt_path)
-
-
-
-
-
-
-
-
-#A partire dalla cartella video salvare in una lista tutti i video contenuti
-
-#for video in videoList chiamando per ciscuno video2Frame e gli passo percorso della cartella video + nome singolo video
-    #Convertire il nome del video nel nome del csv (con basename o replace a mia scelta)
-    #usando basename creo una cartella con il nome video con dentro 2 sottocartelle images, labels(txt)
-    #chiamare video2frame (path_video + nomevideo, pathoutput + images)
-    #faccio e chiamo una nuova fun zione per salvare i txt csv2Txt (path_csv+nome_csv, pathoutput+labels)
-
-
-
-
-
